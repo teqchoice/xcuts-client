ee09a48c21136dcafd5ce38081aae279d572b64e
Mahmoodreza Zakeri <dev@mzakeri.ir>
2024-06-26 22:53:42 +0330

    compres

--- src/views/layout/footer/mineMenu.tsx
+++ src/views/layout/footer/mineMenu.tsx
@@ -1,7 +1,6 @@
 import Link from 'next/link'
 import { select } from 'radash'
 import React from 'react'
-
 export default function MineMenu({ data }: any) {
   // console.log(Collections)
   const Collections = select(
@@ -24,11 +23,30 @@ export default function MineMenu({ data }: any) {
     item => item,
     item => item[0]?.title?.includes('Company')
   )[0]
-  const Resources = select(
-    data,
-    item => item,
-    item => item[0]?.title?.includes('Resources')
-  )[0]
+  const Resources = [
+    {
+      id: '44',
+      title: 'Resources',
+      value: 'Articles',
+      link: '',
+      filec: ''
+    },
+    {
+      id: '45',
+      title: 'Inspiration hub',
+      value: 'Xcuts Tips',
+      link: '',
+      filec: ''
+    },
+    {
+      id: '46',
+      title: 'How to',
+      value: 'FAQ',
+      link: '',
+      filec: ''
+    }
+  ]
+  console.log(Resources)
   // console.log(Collections.slice(1, 7))
   return (
     <div className='ftr__links grid grid-cols-1 md:grid-cols-3 gap-4 lg:grid-cols-6 space-y-14 md:space-y-0 md:space-x-6'>
@@ -93,7 +111,7 @@ export default function MineMenu({ data }: any) {
       </div>
       <div className='basis-full md:basis-[15%]'>
         <div className='w-16 h-[0.35rem] mb-2 bg-primary'></div>
-        <h5 className='text-2xl font-semibold text-white mb-4'>{Resources[0].value}</h5>
+        <h5 className='text-2xl font-semibold text-white mb-4'>Resources</h5>
         <div className='flex gap-x-14'>
           <div className='mt-4 space-y-2 flex flex-col'>
             {Resources?.map((item: any) => (
