cdc0c8bd676b356f4b9f7ecc6bcec626ea56c00e
Mahmoodreza Zakeri <dev@mzakeri.ir>
2024-07-06 22:17:06 +0330

    login page fixed

--- src/views/pages/login/login.tsx
+++ src/views/pages/login/login.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useForm } from '@mantine/form'
 import axios from 'axios'
 import { Lock, Mail } from '@icon-park/react'
@@ -14,21 +14,58 @@ export default function Login() {
   const form = useForm({
     initialValues: {
       email: '',
-      password: ''
+      password: '',
+      otp: ''
     }
   })
+  const [code, setCode] = useState(false)
 
-  function handleSend() {
+  function handleSendCode() {
     // console.log(form.values)
 
     let config = {
       method: 'post',
       maxBodyLength: Infinity,
-      url: 'https://api.xcuts.co.uk/api/login/',
+      url: 'https://api.xcuts.co.uk/api/user/login',
       headers: {
         'Content-Type': 'application/json'
       },
-      data: form.values
+      data: {
+        email: form.values.email,
+        password: form.values.password
+      }
+    }
+
+    axios
+      .request(config)
+      .then(response => {
+        // console.log(response.data)
+
+        toast.success('code was sent to your email')
+        setCode(true)
+        // window.location.replace('/dashboard')
+      })
+      .catch(error => {
+        console.log(error)
+        toast.error(error?.response?.data?.detail)
+      })
+  }
+
+  function handleSubmit() {
+    // console.log(form.values)
+
+    let config = {
+      method: 'post',
+      maxBodyLength: Infinity,
+      url: 'https://api.xcuts.co.uk/api/user/verify',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      data: {
+        email: form.values.email,
+        password: form.values.password,
+        otp: form.values.otp
+      }
     }
 
     axios
@@ -41,11 +78,12 @@ export default function Login() {
           expires: oneWeekFromNow
         })
         toast.success('You have successfully logged in')
+        setCode(false)
         window.location.replace('/dashboard')
       })
       .catch(error => {
         console.log(error)
-        toast.error('No account with this profile was found')
+        toast.error(error?.response?.data?.detail)
       })
   }
 
@@ -58,9 +96,13 @@ export default function Login() {
               <div className='w-full'>
                 <div className='text-center'>
                   <p className='text-3xl text-black'>Sign in</p>
-                  <h3 className='text-3xl font-bold'>Continue to your account</h3>
+                  {!code ? (
+                    <h3 className='text-3xl font-bold'>Continue to your account</h3>
+                  ) : (
+                    <h3 className='text-3xl font-bold'>Enter the code to continue</h3>
+                  )}
                 </div>
-                <form onSubmit={form.onSubmit(handleSend)}>
+                <form onSubmit={form.onSubmit(!code ? handleSendCode : handleSubmit)}>
                   <div className='mb-7 mt-10'>
                     <div className='relative'>
                       <input
@@ -70,25 +112,13 @@ export default function Login() {
                         placeholder='Enter your email address'
                         {...form.getInputProps('email')}
                       />
-                      {/* <TextInput
-                        placeholder='Enter your email address'
-                        // styles={{
-                        //   input: {
-                        //     border: '1px solid #49494940',
-                        //     borderRadius: '3px',
-                        //     width: '100%',
-                        //     padding: '5px',
-                        //     color: '#7e7d7d'
-                        //   }
-                        // }}
-                        {...form.getInputProps('email')}
-                        className='appearance-none rounded-lg h-[92px] w-full py-3 px-4 pl-24 text-gray-700 leading-tight focus:outline-none focus:shadow-outline border-l-8 border-primary drop-shadow-[0_10px_25px_rgba(0,0,0,0.10)]'
-                      /> */}
+
                       <div className='absolute top-1/2 -translate-y-1/2 left-4 w-16 h-16 flex justify-center items-center border-r border-[#d0d0d0]'>
                         <Mail className='cursor-pointer' theme='outline' strokeWidth={2} size='35' />
                       </div>
                     </div>
                   </div>
+
                   <div className='mb-7'>
                     <div className='relative'>
                       <input
@@ -103,14 +133,41 @@ export default function Login() {
                       </div>
                     </div>
                   </div>
+                  {code && (
+                    <div className='mb-7'>
+                      <div className='relative'>
+                        <input
+                          id='otp'
+                          type='text'
+                          className='appearance-none rounded-lg h-[92px] w-full py-3 px-4 pl-24 text-gray-700 leading-tight focus:outline-none focus:shadow-outline border-l-8 border-primary drop-shadow-[0_10px_25px_rgba(0,0,0,0.10)]'
+                          placeholder='enter your code'
+                          {...form.getInputProps('otp')}
+                        />
+                        <div className='absolute top-1/2 -translate-y-1/2 left-4 w-16 h-16 flex justify-center items-center border-r border-[#d0d0d0]'>
+                          <Lock className='cursor-pointer' theme='outline' strokeWidth={2} size='35' />
+                        </div>
+                      </div>
+                    </div>
+                  )}
+
                   <div className='flex items-center justify-center'>
-                    <button
-                      className='bg-black text-white font-medium text-xl py-4 px-14 focus:outline-none focus:shadow-outline inline-block'
-                      // onClick={handleSend}
-                      type='submit'
-                    >
-                      Sign in
-                    </button>
+                    {!code ? (
+                      <button
+                        className='bg-black text-white font-medium text-xl py-4 px-14 focus:outline-none focus:shadow-outline inline-block'
+                        // onClick={handleSend}
+                        type='submit'
+                      >
+                        Sent Code
+                      </button>
+                    ) : (
+                      <button
+                        className='bg-black text-white font-medium text-xl py-4 px-14 focus:outline-none focus:shadow-outline inline-block'
+                        // onClick={handleSend}
+                        type='submit'
+                      >
+                        Sign in
+                      </button>
+                    )}
                   </div>
                   <div className='mt-10 flex justify-between'>
                     <div className='flex items-center space-x-2'>
