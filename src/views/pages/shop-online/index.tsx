c00a347f0ce0d085aabf5a0886528e7317d36024
Mahmoodreza Zakeri <dev@mzakeri.ir>
2024-07-15 15:23:22 +0330

    fix

--- src/views/pages/shop-online/index.tsx
+++ src/views/pages/shop-online/index.tsx
@@ -13,7 +13,7 @@ export default function index({ Data }: any) {
   const rel = 'data-te-nav-active'
   return (
     <>
-      <PageIntroduction Data={{ title: 'Coming Soon', baner: 'images/order-banner.webp' }} />
+      <PageIntroduction Data={{ title: '', baner: 'images/order-banner.webp' }} />
 
       <div className='mt-16'>
         <div className='px-5 overflow-x-hidden'>
@@ -24,12 +24,12 @@ export default function index({ Data }: any) {
           >
             <li role='presentation' className='tab'>
               <a
-                href='#tabs-cutedge'
+                href='#tabs-cnc-cutting'
                 className='my-2 block hover:bg-white data-[te-nav-active]:bg-white hover:shadow-[-4px_3px_6px_-1px_rgba(0,0,0,0.3)] data-[te-nav-active]:shadow-[-4px_3px_6px_-1px_rgba(0,0,0,0.3)] text-lg font-medium text-black leading-tight hover:isolate hover:border-transparent bg-[#bab9b9] transition-all skew-x-[45deg] group'
                 data-te-toggle='pill'
-                data-te-target='#tabs-cutedge'
+                data-te-target='#tabs-cnc-cutting'
                 role='tab'
-                aria-controls='tabs-cutedge'
+                aria-controls='tabs-cnc-cutting'
                 aria-selected='true'
                 onClick={() => setActive(1)}
                 data-te-nav-active={active === 1 ? 'true' : undefined}
@@ -38,7 +38,7 @@ export default function index({ Data }: any) {
                   className='-skew-x-[45deg] bg-no-repeat bg-[left_27px_top_1px]'
                   style={{ backgroundImage: "url('assets/images/order-x-icon.webp')" }}
                 >
-                  <span className='inline-block md:px-8 px-5'>Cut, edge & spray</span>
+                  <span className='inline-block md:px-8 px-5'>CNC cutting</span>
                   <span className='inline-block group-hover:bg-primary bg-[#7c7c7c] md:px-6 px-4 py-5 skew-x-[45deg] data-active'>
                     <span className='-skew-x-[45deg] text-white inline-block'>£0.00</span>
                   </span>
@@ -47,12 +47,12 @@ export default function index({ Data }: any) {
             </li>
             <li role='presentation' className='tab'>
               <a
-                href='#tabs-cnc-cutting'
+                href='#tabs-cutedge'
                 className='my-2 block hover:bg-white data-[te-nav-active]:bg-white hover:shadow-[-4px_3px_6px_-1px_rgba(0,0,0,0.3)] data-[te-nav-active]:shadow-[-4px_3px_6px_-1px_rgba(0,0,0,0.3)] text-lg font-medium text-black leading-tight hover:isolate hover:border-transparent bg-[#bab9b9] transition-all skew-x-[45deg] group'
                 data-te-toggle='pill'
-                data-te-target='#tabs-cnc-cutting'
+                data-te-target='#tabs-cutedge'
                 role='tab'
-                aria-controls='tabs-cnc-cutting'
+                aria-controls='tabs-cutedge'
                 aria-selected='true'
                 onClick={() => setActive(2)}
                 data-te-nav-active={active === 2 ? 'true' : undefined}
@@ -61,7 +61,7 @@ export default function index({ Data }: any) {
                   className='-skew-x-[45deg] bg-no-repeat bg-[left_27px_top_1px]'
                   style={{ backgroundImage: "url('assets/images/order-x-icon.webp')" }}
                 >
-                  <span className='inline-block md:px-8 px-5'>CNC cutting</span>
+                  <span className='inline-block md:px-8 px-5'>Cut, edge & spray</span>
                   <span className='inline-block group-hover:bg-primary bg-[#7c7c7c] md:px-6 px-4 py-5 skew-x-[45deg] data-active'>
                     <span className='-skew-x-[45deg] text-white inline-block'>£0.00</span>
                   </span>
@@ -127,24 +127,24 @@ export default function index({ Data }: any) {
             <div className='mb-6'>
               <div
                 className='hidden opacity-100 transition-opacity duration-150 ease-linear data-[te-tab-active]:block'
-                id='tabs-cutedge'
+                id='tabs-cnc-cutting'
                 role='tabpanel'
                 aria-labelledby='tabs-contact-tab'
                 data-te-tab-active={active === 1 ? 'true' : undefined}
               >
-                <div className='border border-dashed border-gray-400 p-10 text-center font-bold text-xl'>
-                  Coming soon
+                <div className='grid grid-cols-2 gap-5 border border-dashed rounded-md border-gray-400 p-10 text-center font-bold text-xl'>
+                  <CNCCutting />
                 </div>
               </div>
               <div
                 className='hidden opacity-100 transition-opacity duration-150 ease-linear data-[te-tab-active]:block'
-                id='tabs-cnc-cutting'
+                id='tabs-cutedge'
                 role='tabpanel'
                 aria-labelledby='tabs-contact-tab'
                 data-te-tab-active={active === 2 ? 'true' : undefined}
               >
-                <div className='grid grid-cols-2 gap-5 border border-dashed rounded-md border-gray-400 p-10 text-center font-bold text-xl'>
-                  <CNCCutting />
+                <div className='border border-dashed border-gray-400 p-10 text-center font-bold text-xl'>
+                  Coming soon
                 </div>
               </div>
               <div
