596f8199d908b7a13edd52fdbd61dadda84d2f86
Mahmoodreza Zakeri <m.zakeri@jaguars.dev>
2024-09-09 02:45:07 +0330

    .

--- src/views/pages/decor-collections/pupupt.tsx
+++ src/views/pages/decor-collections/pupupt.tsx
@@ -8,121 +8,122 @@ export default function Pupapt() {
       <a className='popup-modal-dismiss absolute top-0 right-2' href='#'>
         <i className='fa-solid fa-xmark' />
       </a>
-      <div className='w-full  h-1/2  p-2 bg-gray-100'>
-        <div className='flex justify-between accordion-contents'>
-          <div className='flex  w-1/2 pl-4'>
-            <div className='relative'>
-              <p>ProductCode :F12C-PV-19</p>
-              <img src='/images/img-1.jpeg' className='w-100  h-60 ' />
-              <div className='absolute bottom-3 left-0 '>
-                <button className='border-2 border-red-700 bg-red-700 p-2 w-32 text-white'>Decor</button>
-              </div>
+      <div className='w-full px-6 py-2 bg-gray-100'>
+        <p>ProductCode :F12C-PV-19</p>
+        <div className='grid grid-cols-2'>
+          <div className='col-span-1 grid grid-cols-12 gap-1'>
+            <div className='col-span-9 relative overflow-hidden'>
+              <img src='/images/img-1.jpeg' className='w-full h-full' />
+              <div className='absolute z-10 py-3 pl-5 pr-[75px] before:border-solid before:border-t-[bg-transparent] before:border-r-transparent before:border-b-primary before:border-l-[bg-transparent] before:border-t-0 sm:before:border-r-[40px] before:border-b-[40px] before:border-l-0 before:absolute before:w-full before:h-full before:-top-2 before:md:top-0 before:left-0 before:-z-10 lg:bottom-0 bottom-0'>
+                <div className='flex justify-between'>
+                  <span className='font-medium text-white mr-4'>Decor</span>
+                </div>
+              </div>   
             </div>
-            <div className='pt-4 p-4'>
-              <img src='/images/img-2.jpeg' className='w-20  h-20  pt-3' />
-              <img src='/images/img-3.jpeg' className='w-20 h-20  pt-3' />
-              <img src='/images/img-4.webp' className='w-20 h-20  pt-3' />
+            <div className='col-span-3 flex flex-col gap-1'>
+              <img src='/images/img-2.jpeg' className='w-full h-full' />
+              <img src='/images/img-3.jpeg' className='w-full h-full' />
+              <img src='/images/img-4.webp' className='w-full h-full' />
             </div>
           </div>
-          <div className='w-1/2 mr-7 pt-6'>
-            <h3 className='text-xl text-red-600'>Egger f120PM</h3>
-            <p className='text-sm text-gray-400'>Light Grey Meta Rock</p>
-            <h5 className='text-xl text-black font-bold mt-2'>Product characteristics</h5>
+          <div className='col-span-1 px-4'>
+            <h3 className='text-2xl font-bold text-primary'>Egger f120PM</h3>
+            <p className='text-sm text-gray-500'>Light Grey Meta Rock</p>
+            <h5 className='text-lg text-black font-bold mt-2'>Product characteristics</h5>
             <p className='text-sm  text-gray-400'>vsegewehytjjhegffff</p>
             <p className='text-sm  text-gray-400'>vsegewehytjjhegffff</p>
             <p className='text-sm  text-gray-400'>vsegewehytjjhegffff</p>
-            <h5 className='text-xl text-black font-bold mt-2'>B side description</h5>
+            <h5 className='text-lg text-black font-bold mt-2'>B side description</h5>
             <p className='text-sm  text-gray-400'>qwegw fe wg bgsf g g ds af afs ad a</p>
             <div className='flex items-center text-center mt-2'>
-              <h5 className='text-base  text-black font-bold mr-4 text-center'>Available download</h5>
-              <button className='flex border-2  border-gray-400 p-2 text-red-700  text-xs align-center w-32 h-8 items-center text-center   bg-white'>
+              <h5 className='text-lg  text-black font-bold mr-3 text-center'>Available download</h5>
+              <a href='#' download className='flex border  border-gray-400 px-2 py-1 text-primary text-xs font-semibold align-center items-center text-center bg-white rounded-lg'>
                 PDF Documents
-              </button>
+              </a>
             </div>
           </div>
           <button className='flex align-start '>{/* <IoCloseSharp /> */}</button>
         </div>
         <div className='flex align-center justify-center text-center'>
-          <button className='p-4 mb-2 w-52 outline-red-700 mt-2  text-xs   text-white bg-red-700'>
+          <button className='px-4 py-3 mb-2 w-52 outline-primary mt-2 text-xs text-white bg-primary'>
             Order a sample for $3.co
           </button>
         </div>
       </div>
 
-      <div className='flex md:flex justify-between w-full h-1/2 p-2'>
-        <div className='w-2/6 ml-4 h-82 text-sm'>
-          <div className='h-2/4 ml-3 mt-1 bg-gray-100 border-2 border-gray-400 p-4 mb-1'>
+      <div className='w-full px-6 py-2 grid grid-cols-12 gap-x-4'>
+        <div className='col-span-5 flex flex-col gap-2'>
+          <div className='bg-gray-100 border border-gray-400/70 p-4 rounded-lg'>
             <h5 className='text-xl font-bold'>Product Maching</h5>
             <div className='flex justify-between'>
-              <p className='text-red-700'>Cut to Size</p>
+              <p className='text-primary'>Cut to Size</p>
               <span>Yse</span>
             </div>
             <div className='flex justify-between'>
-              <p className='text-red-700'>Cut to Size</p>
+              <p className='text-primary'>Cut to Size</p>
               <span>Yse</span>
             </div>
             <div className='flex justify-between'>
-              <p className='text-red-700'>Cut to Size</p>
+              <p className='text-primary'>Cut to Size</p>
               <span>Yse</span>
             </div>
             <div className='flex justify-between'>
-              <p className='text-red-700'>Cut to Size</p>
+              <p className='text-primary'>Cut to Size</p>
               <span>Yse</span>
             </div>
           </div>
-          <div className='h-1/3  ml-3 mt-2 bg-gray-100 border-2 border-gray-400 p-4  mb-2'>
+          <div className='bg-gray-100 border border-gray-400/70 p-4 rounded-lg'>
             <h5 className='text-xl font-bold'>Product size</h5>
             <div className='flex justify-between'>
-              <p className='text-red-700'>Cut to Size</p>
+              <p className='text-primary'>Cut to Size</p>
               <span>Yse</span>
             </div>
             <div className='flex justify-between'>
-              <p className='text-red-700'>Cut to Size</p>
+              <p className='text-primary'>Cut to Size</p>
               <span>Yse</span>
             </div>
           </div>
         </div>
-        <div className=' w-4/6 ml-8 p-4'>
-          <div>
-            <button className='border-2 border-lg border-gray-400 p-1 mr-3'>
-              {' '}
+        <div className='col-span-7'>
+          <div className='flex gap-3 items-center'>
+            <button className='flex items-center justify-center border w-8 h-8 p-1 bg-white  border-gray-400/70 rounded-md'>
               <FaPlus />
             </button>
             <span className='text-sm font-bold'>1</span>
-            <button className='border-2 border-lg border-gray-400 p-1 ml-3'>
+            <button className='flex items-center justify-center border w-8 h-8 p-1 bg-white  border-gray-400/70 rounded-md'>
               <FaMinus />
             </button>
           </div>
-          <div className='flex  mt-3'>
-            <div className='mr-4 '>
-              <button className='p-3 outline-red-700 mt-2 w-40  text-xs   text-white bg-red-700'>Buy full sheet</button>
-              <h3 className='text-2xl font-bold'>
-                Total:<span className='text-red-700'>$248.75</span>
+          <div className='flex w-full gap-5'>
+            <div className=' w-full'>
+              <button className='p-3 outline-primary mt-2 w-full text-xs text-white bg-primary'>Buy full sheet</button>
+              <h3 className='text-2xl'>
+                Total:<span className='text-primary font-bold'>$248.75</span>
               </h3>
               <p className='text-sm'>With VAI:$298.50</p>
             </div>
-            <div className='ml-4'>
-              <button className='p-3 outline-black mt-2 w-40  text-xs   text-white bg-black'>Add to cutting list</button>
-              <h3 className='text-2xl font-bold'>
-                Total:<span className='text-red-700'>$248.75</span>
+            <div className=' w-full'>
+              <button className='p-3 outline-black mt-2 w-full text-xs text-white bg-black'>Add to cutting list</button>
+              <h3 className='text-2xl'>
+                Total:<span className='text-primary font-bold'>$248.75</span>
               </h3>
               <p className='text-sm'>With VAI:$298.50</p>
             </div>
           </div>
-          <div className='mt-4'>
-            <p className='text-sm'>Available thicknesses[mm]:</p>
-            <div className='border-2 border-gray-400 p-2 bg-gray-100 mt-2'>
-              <div className='p-2'>
-                <span className='border-2 bg-red-700 border-red-700 p-1 text-white text-sm'>18</span>
-                <span className='border-2 bg-white border-red-700 p-1 text-black text-sm ml-1'>38</span>
+          <div className='mt-2'>
+            <p className='text-xs mb-1'>Available thicknesses[mm]:</p>
+            <div className='bg-gray-100 border border-gray-400/70 p-2 rounded-lg'>
+              <div className='flex gap-2'>
+                <span className='flex items-center justify-center border w-8 h-8 p-1 bg-primary border-primary text-white '>18</span>
+                <span className='flex items-center justify-center border w-8 h-8 p-1 bg-white border-primary text-black'>38</span>
               </div>
               <div className='flex justify-between'>
-                <p className='text-sm font-bold'>Full sheet Price</p>
-                <span className='text-red-700'>$248.75</span>
+                <p className='text-sm text-black'>Full sheet Price</p>
+                <span className='text-primary'>$248.75</span>
               </div>
               <div className='flex justify-between'>
-                <p className='text-sm font-bold'>Full sheet Price</p>
-                <span className='text-red-700'>$248.75</span>
+                <p className='text-sm text-black'>Full sheet Price</p>
+                <span className='text-primary'>$248.75</span>
               </div>
             </div>
           </div>
