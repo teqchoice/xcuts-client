ee2c223e92584034539f27043a0c717f4f7a78be
Mahmoodreza Zakeri <m.zakeri@jaguars.dev>
2024-09-09 05:13:44 +0330

    .

--- next.config.js
+++ next.config.js
@@ -5,19 +5,19 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true
   },
-  images: {
-    // unoptimized: true,
-    // loader: 'custom',
-    // loaderFile: './src/views/my-loader.tsx',
-    remotePatterns: [
-      {
-        protocol: `${process.env.NEXT_PUBLIC_HTTP}`,
-        hostname: `${process.env.NEXT_PUBLIC_DOMIN}`,
-        port: `${process.env.NEXT_PUBLIC_PORT}`,
-        pathname: '/**'
-      }
-    ]
-  }
+  // images: {
+  //   // unoptimized: true,
+  //   // loader: 'custom',
+  //   // loaderFile: './src/views/my-loader.tsx',
+  //   remotePatterns: [
+  //     {
+  //       protocol: `${process.env.NEXT_PUBLIC_HTTP}`,
+  //       hostname: `${process.env.NEXT_PUBLIC_DOMIN}`,
+  //       port: `${process.env.NEXT_PUBLIC_PORT}`,
+  //       pathname: '/**'
+  //     }
+  //   ]
+  // }
 }
 
 module.exports = nextConfig
