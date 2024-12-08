94247fa4c61f40b5e579c9795b54995195d445d5
sarok <140957347+sarok86@users.noreply.github.com>
2024-12-08 03:53:33 -0800

    .

--- src/views/layout/header/topMenu.tsx
+++ src/views/layout/header/topMenu.tsx
@@ -11,13 +11,16 @@ export default function TopMenu({ data }: any) {
   return (
     <div className='relative bg-neutral-300'>
       <div className='px-4 sm:container flex justify-center sm:justify-end p-3 relative divide-x-2'>
-        <div className='group relative ' key={data?.id}>
-          <Link href='#' className='relative z-20 text-sm sm:text-base/3 text-white font-medium px-2 py-1'>
-            {data?.my_account}
-          </Link>
-          <div className='opacity-0 z-10 group-hover:opacity-100 transition-all duration-150 w-16 h-8 bg-primary absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 skew-y-[0] skew-x-[35deg]'></div>
-        </div>
-        <div className='group relative ' key={data?.id}>
+        {data?.top_menu_items.map((e: any) => (
+          <div className='group relative ' key={data?.id}>
+            <Link href={e?.link} className='relative z-20 text-sm sm:text-base/3 text-white font-medium px-2 py-1'>
+              {e?.name}
+            </Link>
+            <div className='opacity-0 z-10 group-hover:opacity-100 transition-all duration-150 w-16 h-8 bg-primary absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 skew-y-[0] skew-x-[35deg]'></div>
+          </div>
+        ))}
+
+        {/* <div className='group relative ' key={data?.id}>
           <Link href='#' className='relative z-20 text-sm sm:text-base/3 text-white font-medium px-2 py-1'>
             {data?.samples}
           </Link>
@@ -34,7 +37,7 @@ export default function TopMenu({ data }: any) {
             {data?.delivery}
           </Link>
           <div className='opacity-0 z-10 group-hover:opacity-100 transition-all duration-150 w-16 h-8 bg-primary absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 skew-y-[0] skew-x-[35deg]'></div>
-        </div>
+        </div> */}
       </div>
       {user_info?.role === 'admin' && process.env.NEXT_PUBLIC_ADMIN_SHOW === 'true' && (
         <div className='cursor-pointer flex items-center justify-center bg-primary border border-white p-4 w-fit absolute left-0 top-0'>
